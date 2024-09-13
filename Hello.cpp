c10de43a05a3cc2d6e2616bce6b686e71d43f051
norfolkak <st130153@student.spbu.ru>
2024-09-14 02:17:00 +0300

    добавил readme

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+/*
+Aleksei Klopov st130153@student.spbu.ru
+Задача №1
+*/
+
+
+#include <iostream>
+
+int main(int, argc, char, *argv[])
+{
+	std :: cout<<"Hello, world"<<std::endl;
+	return 0
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Demo project 1
 
+# Author
+Klopov Aleksei Romanovich, 24.B82-mm
+
+st130153@student.spbu.ru
+
+# Build
+
+make
+
+# Run
