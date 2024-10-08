f5233d78d664bfba89d07f5e05ad9882d59de616
norfolkak <st130153@student.spbu.ru>
2024-10-08 19:41:16 +0300

    addea an array

--- Assignment2a/main.cpp
+++ Assignment2a/main.cpp
@@ -8,9 +8,13 @@ int main()
     std::ifstream infile;
     infile.open("readerdata.txt",std::ios::binary|std::ios::in);
     
-    uintmax_t file_size {};
-    std::filesystem::file_size("readerdata.txt");
+    uintmax_t file_size {std::filesystem::file_size("readerdata.txt")-1};
     std::cout << file_size << '\n';
+    char *buffer {new char[file_size]};
+    infile.read((char *)buffer,file_size);
+    std::cout << buffer << '\n';
+    
     infile.close();
+    
 
 }
