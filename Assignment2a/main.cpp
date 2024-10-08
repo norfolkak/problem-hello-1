a74ff1bba449f4a278835c248634cd10556e47c5
norfolkak <st130153@student.spbu.ru>
2024-10-08 20:36:41 +0300

    made main

--- Assignment2a/main.cpp
+++ Assignment2a/main.cpp
@@ -7,14 +7,24 @@ int main()
 {    
     std::ifstream infile;
     infile.open("readerdata.txt",std::ios::binary|std::ios::in);
+    std::ofstream outfile;
+    outfile.open("reverseddata.txt",std::ios::binary|std::ios::out);
     
     uintmax_t file_size {std::filesystem::file_size("readerdata.txt")-1};
     std::cout << file_size << '\n';
     char *buffer {new char[file_size]};
     infile.read((char *)buffer,file_size);
-    std::cout << buffer << '\n';
+    
+    char temporary{};
+    for (int i=0; i<file_size/2; i++)
+    {
+    temporary=buffer[i];
+    buffer[i]=buffer[file_size-1-i];
+    buffer[file_size-1-i]=temporary;
+    }
+    outfile.write((char *)buffer,file_size);
+    delete[] buffer;
     
     infile.close();
-    
-
+    outfile.close();
 }
