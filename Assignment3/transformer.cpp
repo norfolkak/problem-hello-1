14666edb2b8e49215be310a710af76f5e86fdfeb
norfolkak <st130153@student.spbu.ru>
2024-10-24 03:53:03 +0300

    added transformer base clase and a composition with a weapon

--- Assignment3/transformer.cpp
+++ Assignment3/transformer.cpp
@@ -1,3 +1,16 @@
 //Klopov Aleksei - Assignment 2b - st130153@student.spbu.ru
 
+#include "transformer.h"
+
+Transformer::Transformer(const std::string& name, const std::string& faction,
+                         int strength, int speed, const Weapon& weapon)
+    : name(name), faction(faction), strength(strength), 
+      speed(speed), weapon(weapon) {}
+
+Transformer::~Transformer() {}
+
+std::string Transformer::getName() const { return name; }
+std::string Transformer::getFaction() const { return faction; }
+int Transformer::getStrength() const { return strength; }
+int Transformer::getSpeed() const { return speed; }
 
--- Assignment3/transformer.h
+++ Assignment3/transformer.h
@@ -6,6 +6,26 @@
 #include "weapon.h"
 #include <string>
 
+class Transformer {
+private:
+    std::string name;
+    std::string faction;
+    int strength;
+    int speed;
+    Weapon weapon;
 
+protected:
+    Transformer(const std::string& name, const std::string& faction,
+                int strength, int speed, const Weapon& weapon);
+    virtual ~Transformer();
+
+    std::string getName() const;
+    std::string getFaction() const;
+    int getStrength() const;
+    int getSpeed() const;
+
+    virtual void transform() = 0;
+    virtual void attack() = 0;
+};
 
 #endif
